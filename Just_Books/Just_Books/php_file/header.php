550d66a5e9c1a7b75528c3ae7de452f8bc00762d
Maithreyi Pagar <pagarmn@rknec.edu>
2020-05-14 19:38:17 +0530

    Initial Commit

---
+++ Just_Books/Just_Books/php_file/header.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
+		<link rel="stylesheet" type="text/css" href="css/header_style.css" />
+	</head>
+	<body>
+		<header>
+			<a href="./">
+				<div id="cd-logo">
+				<h1>	<center><b>JUST BOOKS</b></center></h1>
+				</div>
+			</a>
+		</header>
+	</body>
+</html>
